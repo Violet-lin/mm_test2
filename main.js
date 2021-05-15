a5a7fb190f8971f6c7218fcf09f79a1ed4f63f30
Violet Lin <p0449201@gmail.com>
2021-05-15 16:39:11 +0800

    add: arrow function

---
+++ main.js
@@ -0,0 +1,38 @@
+var slide;
+var numSlides;
+var slideWidth = 300;
+var slideDelay = 3;
+var slideDuration = 0.3;
+
+var snapX;
+var current = 0;
+
+var prevButton = document.querySelector("#arrow_prev");
+var nextButton = document.querySelector("#arrow_next");
+
+var slides01 = document.querySelector("#slides");
+var slides02 = document.querySelectorAll(".slide");
+var slides03 = document.querySelector(".slide");
+var logos = document.getElementById("logo");
+
+console.log("current " + current)
+
+prevButton.addEventListener("click", function () {
+    current--; 
+    animateSlides(1);
+  });
+  
+nextButton.addEventListener("click", function () {
+    current++;
+    animateSlides(-1);
+  });
+
+
+function animateSlides(direction) {
+
+    snapX = -current * slideWidth;
+    console.log("current " + current)
+    console.log(snapX)
+    TweenLite.to(slides03, 1, { x: snapX });
+
+}
